73286b1d5bf5b8788b4ea2ac4e9aaae0aa52851a
曾弘扬 <cenghongyang@cenghongyangdeMacBook-Air.local>
2020-08-05 15:47:17 +0800

    filter

---
+++ src/main/java/com/zeng/filter/CorsFilter.java
@@ -0,0 +1,41 @@
+package com.zeng.filter;
+
+import org.apache.http.HttpStatus;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CorsFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        //指定允许其他域名访问
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+
+        //响应头设置
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        //响应类型
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+
+        if ("OPTIONS".equals(httpServletRequest.getMethod())) {
+            httpServletResponse.setStatus(HttpStatus.SC_NO_CONTENT);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
